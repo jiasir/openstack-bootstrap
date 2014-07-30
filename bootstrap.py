f848aab9d97b415315a47ab6114638f84ba9bc3e
jiasir <jiasir@icloud.com>
2014-07-30 20:53:59 +0800

    A bit code

---
+++ bootstrap.py
@@ -0,0 +1,19 @@
+__author__ = 'taio'
+
+from command import Command
+
+def usage():
+	print 'Usage: python bootstrap.py'
+
+def juju_deploy():
+	pass
+
+def add_relation():
+	pass
+
+def maas_deploy():
+	pass
+
+def get_maas_api():
+	pass
+
